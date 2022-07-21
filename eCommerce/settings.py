0d8b90f02d45ef54b57e56f1623b1cefcb7ec601
el-elliven <86650329+el-elliven@users.noreply.github.com>
2022-07-21 10:45:12 +0300

    new requirement

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -144,3 +144,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
