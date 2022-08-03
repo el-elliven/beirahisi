136f4de76a7cacd16b234ae9b483ce0364aafd33
el-elliven <86650329+el-elliven@users.noreply.github.com>
2022-08-04 00:50:48 +0300

    settingss

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -81,10 +81,23 @@ WSGI_APPLICATION = 'eCommerce.wsgi.application'
 #
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config("DATABASE_ENGINE"),
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config("DATABASE_NAME"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': 5432,
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
